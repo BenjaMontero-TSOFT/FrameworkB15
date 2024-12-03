ecbc93a13081013a190fdc82f82770805331654e
Gonzalo <gonzalo.acevedo@tsoftglobal.com>
2024-12-03 14:51:10 -0300

    Actualizo metodos en searchNavigationTrenes

--- src/test/java/aut/testcreation/pages/trenes/FiltersJourney.java
+++ src/test/java/aut/testcreation/pages/trenes/FiltersJourney.java
@@ -1,14 +1,17 @@
 package aut.testcreation.pages.trenes;
 
 import framework.engine.selenium.SeleniumWrapper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class FiltersJourney extends SeleniumWrapper {
 
     @FindBy(xpath = "(//DIV[@class='FullTransportWay__SummaryGrid-sc-acha5w-1 jouvfC'])[1]")
-    private WebElement Setjourney;
+    private WebElement setJourney;
 
     @FindBy(xpath = "//button[@data-testid='lmn-ds-btn']")
     private WebElement btnSelect;
